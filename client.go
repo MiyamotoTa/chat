af591407068506b87d017dd50b36a67ae0a55e14
miyamoto_ta <miyamoto_ta@zoho.com>
2017-08-30 00:12:09 +0900

    First commit

---
+++ client.go
@@ -0,0 +1,33 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+// clientはチャットを行っている一人のユーザ
+type client struct {
+	// このクライアントのためのWebSocket
+	socket *websocket.Conn
+	// メッセージが送られるチャネル
+	send chan []byte
+	// このクライアントが所属しているチャットルーム
+	room *room
+}
+
+func (c *client) read() {
+	for {
+		if _, msg, err := c.socket.ReadMessage(); err == nil {
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err == nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
