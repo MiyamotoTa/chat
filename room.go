b6cd2759545c9e65187e2d0fb8f5f26b8a4ccda4
miyamoto_ta <miyamoto_ta@zoho.com>
2017-08-31 09:20:43 +0900

    Modeling ChatRoom

--- room.go
+++ room.go
@@ -3,4 +3,36 @@ package main
 type room struct {
 	// 他のクライアントに転送するためのメッセージを保持するチャネル
 	forward chan []byte
+	// チャットルームに参加しようとしているクライアントのためのチャネル
+	join chan *client
+	// チャットルームから退出しようとしているクライアントのためのチャネル
+	leave chan *client
+	// 在籍しているすべてのクライアントが保持される
+	clients map[*client]bool
+}
+
+func (r *room) run() {
+	for {
+		select {
+		case client := <-r.join:
+			//参加
+			r.clients[client] = true
+		case client := <-r.leave:
+			// 退出
+			delete(r.clients, client)
+			close(client.send)
+		case msg := <-r.forward:
+			// すべてのクライアントにメッセージ転送
+			for client := range r.clients {
+				select {
+				case client.send <- msg:
+					// メッセージを送信
+				default:
+					// 送信に失敗
+					delete(r.clients, client)
+					close(client.send)
+				}
+			}
+		}
+	}
 }
