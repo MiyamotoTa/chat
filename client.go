31b0cb365c8a62f95bc66e8608e95cec1d700322
miyamoto_ta <miyamoto_ta@zoho.com>
2017-10-06 21:01:03 +0900

    Display sender name

--- client.go
+++ client.go
@@ -1,20 +1,28 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
 
 // clientはチャットを行っている一人のユーザ
 type client struct {
 	// このクライアントのためのWebSocket
 	socket *websocket.Conn
 	// メッセージが送られるチャネル
-	send chan []byte
+	send chan *message
 	// このクライアントが所属しているチャットルーム
 	room *room
+	// userDataはユーザに関するデータを保持する
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	for {
-		if _, msg, err := c.socket.ReadMessage(); err == nil {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
 			c.room.forward <- msg
 		} else {
 			break
@@ -25,7 +33,7 @@ func (c *client) read() {
 
 func (c *client) write() {
 	for msg := range c.send {
-		if err := c.socket.WriteMessage(websocket.TextMessage, msg); err != nil {
+		if err := c.socket.WriteJSON(msg); err != nil {
 			break
 		}
 	}
